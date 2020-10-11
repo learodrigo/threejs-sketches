e5e323cb06d66f3b0c5c47f2c165b1c999e9cc6f
Leandro Rodrigo <leandrorodrigobs@gmail.com>
2020-10-11 17:23:13 +0200

    Handling responsive design and folder name

--- hello-cubes/sketch.js
+++ hello-cubes/sketch.js
@@ -1,11 +1,24 @@
 import * as THREE from 'https://threejsfundamentals.org/threejs/resources/threejs/r119/build/three.module.js'
 
 const main = () => {
+    // Default canvas size 300x150
     const canvas = document.getElementById('canvas')
-    // canvas.width = window.innerWidth
-    // canvas.height = window.innerHeight
-
     const renderer = new THREE.WebGLRenderer({ canvas })
+
+    const resizeRendererToDisplaySize = (renderer) => {
+        const canvas = renderer.domElement
+        const pixelRatio = window.devicePixelRatio
+        const width = canvas.clientWidth * pixelRatio | 0
+        const height = canvas.clientHeight * pixelRatio | 0
+        const needResize = canvas.width !== width || canvas.height !== height
+
+        if (needResize) {
+            // The last param is to not change CSS properties
+            renderer.setSize(width, height, false)
+        }
+
+        return needResize
+    }
 
     // Field of view (angle) - three uses randians but for camera takes degrees
     const fov = 75
@@ -46,6 +59,12 @@ const main = () => {
 
     const render = (time) => {
         time *= 0.001
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement
+            camera.aspect = canvas.clientWidth / canvas.clientHeight
+            camera.updateProjectionMatrix()
+        }
 
         cubes.forEach((cube, ndx) => {
             const speed = 1 + ndx * 0.1
